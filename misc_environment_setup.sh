299f16d884242ab5b9152e9586bb293d7b6477b4
Sam Bao <sambao21@gmail.com>
2013-09-12 09:24:24 -0500

    More environment setup

---
+++ misc_environment_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  set_up_aquamacs.sh
+#  
+#
+#  Created by Tyler Young on 9/12/13.
+#
+chflags nohidden ~/Library
+chflags nohidden /Library
+
+echo "alias ls='ls -GpFh'" >> ~/.bash_profile
+echo "" >> ~/.bash_profile
+
+
+
---
+++ set_up_aquamacs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  set_up_aquamacs.sh
+#  
+#
+#  Created by Tyler Young on 9/12/13.
+#
+cp -R ~/Dropbox/Aquamacs\ Emacs/. ~/Library/Preferences/Aquamacs\ Emacs/
+
+
+
