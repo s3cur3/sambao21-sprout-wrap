cdda37ce55c0dee2397e8951490928618cdc7642
Sam Bao <sambao21@gmail.com>
2013-11-15 10:23:39 -0600

    Better notifications, script to reposition KeepassX from offscreen

--- run_after_soloist.sh
+++ run_after_soloist.sh
@@ -5,23 +5,42 @@
 #
 #  Created by Tyler Young on 9/12/13.
 #
+echo "Installing Skype"
 brew cask install skype --force
+echo "Installing IntelliJ"
 brew cask install intellij-ultimate --force
+echo "Installing RubyMine"
 brew cask install rubymine --force
+echo "Installing VLC"
 brew cask install vlc --force
+echo "Installing Inkscape"
 brew cask install inkscape --force
+echo "Installing Chicken of the VNC"
 brew cask install chicken --force
+echo "Installing Transmission"
 brew cask install transmission --force
+echo "Installing PHPStorm"
 brew cask install phpstorm --force
+echo "Installing Quicksilver"
 brew cask install quicksilver --force
+echo "Installing Keepass-X"
 brew cask install keepass-x --force
+echo "Installing Logitech Unifying software"
 brew cask install logitech-unifying --force
+echo "Installing Opera"
 brew cask install opera --force
+echo "Installing Pandoc"
 brew cask install pandoc --force
+echo "Installing Pycharm"
 brew cask install pycharm --force
+echo "Installing SABNZBD+"
 brew cask install sabnzbd --force
+echo "Installing VMWare Fusion"
 brew cask install vmware-fusion --force
+echo "Installing Daisy Disk"
 brew cask install daisy-disk
 
+echo "Installing LESS"
+npm install less
 
 
